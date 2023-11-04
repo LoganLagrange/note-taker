61bf8afa55e4ec74b63ffabfeb0de862b7c24a80
Logan Lagrange <lagrangelogan@gmail.com>
2023-11-04 15:19:22 -0400

    Added post request to api/notes

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
+const crypto = require(`crypto`);
 
 router.get(`/api/notes`,(req,res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"))
     console.log(`${req.method} request to ${req.url}`)
     res.json(notes);
+})
+
+router.post(`/api/notes`,(req,res) => {
+    const newNote = {
+        id:crypto.randomUUID(),
+        title:req.body.title,
+        text:req.body.text
+    }
+    const storedNotesData = JSON.parse(fs.readFileSync("./db/db.json"));
+    storedNotesData.push(newNote);
+    fs.writeFileSync("./db/db.json",JSON.stringify(storedNotesData,null,4));
+    console.log(`${req.method} request to ${req.url}`);
+    res.json(newNote);
 })
 
 module.exports = router;
